bebb65b668f54bc15f7b2a57b3fab0ef6d465568
RaspberryPiIndia <33364585+RaspberryPiIndia@users.noreply.github.com>
2018-04-11 18:30:39 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,21 @@
 # INSTALLATION WILL START SHORTLY
 
 sudo apt-get update --fix-missing
-sudo apt-get install totem wireshark vlc aircrack-ng wifite conky wicd blueman uget transmission firefox-esr zenmap nmap hydra netsniff-ng
+sudo apt-get install totem wireshark vlc aircrack-ng wifite conky wicd blueman uget transmission firefox-esr zenmap nmap audacity hydra netsniff-ng
 sudo apt-get upgrade
 sudo apt-get update
 
 # APT-GET DONE
 
-sudo mv /hardware/RAMup.sh 
+sudo mv /Hardware/RAMup.sh /usr/bin/
+sudo chmod +x /usr/bin/RAMup.sh
+sudo nano 
+
+# RAMup DONE
+SET LOGIN PASSWORD
+sudo passwd pi
+SET ROOT PASSWORD
+sudo passwd root
+ DONE
+
+
