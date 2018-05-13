b7d8443c2f99151a926e416388b3075981f5c205
RaspberryPiIndia <33364585+RaspberryPiIndia@users.noreply.github.com>
2018-05-13 07:03:13 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,21 +2,24 @@ echo "WELCOME TO THE COMPLETE RPI 3 INSTALLER";
 echo "INSTALLATION WILL START SHORTLY";
 
 sudo apt-get update --fix-missing
-sudo apt-get install totem wireshark vlc aircrack-ng wifite conky wicd blueman uget transmission firefox-esr zenmap nmap audacity hydra netsniff-ng
+sudo apt-get install totem wireshark aircrack-ng wifite conky wicd blueman figlet android-sdk uget transmission firefox-esr vlc kismet zenmap nmap audacity hydra netsniff-ng gcc g++ make mpg123
 sudo apt-get upgrade
 sudo apt-get update
 
-echo "APT-GET DONE";
+sudo figlet Apt-Get Done!
 
 sudo mv Hardware/RAMup.sh /usr/bin/
 sudo chmod +x /usr/bin/RAMup.sh
+sudo rmdir /etc/rc.local
+sudo mv hardware/rc.local /etc/rc.local
+sudo figlet RAMup DONE!
 
 sudo cp hardware/.conkyrc /home/pi/
 sudo cp hardware/conky.sh /usr/bin/
 sudo cp hardware/conky.desktop /etc/xdg/autostart/
-echo "CONKY DONE";
+sudo figlet CONKY DONE
 
-# RAMup DONE
+
 echo "SET LOGIN PASSWORD";
 sudo passwd pi
 echo "SET ROOT PASSWORD";
