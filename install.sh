34fb5083d658766d4ecddcc73435c1028fac30b2
RaspberryPiIndia <33364585+RaspberryPiIndia@users.noreply.github.com>
2018-05-14 08:54:42 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,28 +2,39 @@ echo "WELCOME TO THE COMPLETE RPI 3 INSTALLER";
 echo "INSTALLATION WILL START SHORTLY";
 
 sudo apt-get update --fix-missing
-sudo apt-get install totem wireshark aircrack-ng wifite conky wicd blueman figlet android-sdk uget transmission firefox-esr vlc kismet zenmap nmap audacity hydra netsniff-ng gcc g++ make mpg123
-sudo apt-get upgrade
-sudo apt-get update
+sudo apt-get install totem wireshark aircrack-ng wifite conky wicd blueman figlet uget transmission firefox-esr vlc kismet zenmap nmap audacity hydra netsniff-ng gcc g++ make mpg123 -y
+sudo apt-get upgrade -y
+sudo apt-get update -y
+sudo apt-get autoremove -y
 
 sudo figlet Apt-Get Done!
+clear
 
 sudo mv RAMup.sh /usr/bin/
 sudo chmod +x /usr/bin/RAMup.sh
 sudo rmdir /etc/rc.local
 sudo mv rc.local /etc/rc.local
 sudo figlet RAMup DONE!
+clear
 
 sudo cp .conkyrc /home/pi/
 sudo cp conky.sh /usr/bin/
 sudo cp conky.desktop /etc/xdg/autostart/
 sudo figlet CONKY DONE
+clear
 
-
+echo "ENTER HOSTNAME : $2"
+sudo hostnamectl set-hostname $2
 echo "SET LOGIN PASSWORD";
 sudo passwd pi
 echo "SET ROOT PASSWORD";
 sudo passwd root
 echo "DONE";
-sudo reboot
+echo "Would you like to reboot now? : $answer"
+if ["$answer" == y]; then
+    sudo reboot
+else 
+    exit 0
+fi
+
 
